926a105203517924b7e6a94807853545c4f057fc
Oerlinker <oerlinker@gmail.com>
2025-04-30 21:54:42 -0400

    Production 2.3.1 Async fix

--- src/main/java/com/clinicaoftalmologica/clinicaoftalmologicabackend/aop/BitacoraAspect.java
+++ src/main/java/com/clinicaoftalmologica/clinicaoftalmologicabackend/aop/BitacoraAspect.java
@@ -1,13 +1,13 @@
 package com.clinicaoftalmologica.clinicaoftalmologicabackend.aop;
 
 import com.clinicaoftalmologica.clinicaoftalmologicabackend.model.Usuario;
+import com.clinicaoftalmologica.clinicaoftalmologicabackend.repository.UsuarioRepository;
 import com.clinicaoftalmologica.clinicaoftalmologicabackend.service.BitacoraService;
-import com.clinicaoftalmologica.clinicaoftalmologicabackend.service.UsuarioService;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
@@ -19,68 +19,71 @@ import java.util.Arrays;
 public class BitacoraAspect {
 
     private final BitacoraService bitacoraService;
-    private final UsuarioService usuarioService;
+    private final UsuarioRepository usuarioRepository;
 
     public BitacoraAspect(BitacoraService bitacoraService,
-                          UsuarioService usuarioService) {
-        this.bitacoraService = bitacoraService;
-        this.usuarioService = usuarioService;
+                          UsuarioRepository usuarioRepository) {
+        this.bitacoraService    = bitacoraService;
+        this.usuarioRepository  = usuarioRepository;
     }
 
-    @Around("@annotation(loggable)")
+
+    @Around("@annotation(loggable) && " +
+            "!execution(* com.clinicaoftalmologica.clinicaoftalmologicabackend.service.UsuarioService.obtenerPorUsername(..))")
     public Object logAround(ProceedingJoinPoint jp, Loggable loggable) throws Throwable {
 
         Object result = jp.proceed();
 
 
-        String username = SecurityContextHolder.getContext()
-                .getAuthentication()
-                .getName();
-        Usuario usuario = usuarioService.obtenerPorUsername(username);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Usuario usuario = null;
+        if (auth != null && auth.isAuthenticated() && !"anonymousUser".equals(auth.getPrincipal())) {
+            String username = auth.getName();
+            usuario = usuarioRepository
+                    .findByUsername(username)
+                    .orElse(null);
+        }
 
 
         Object[] args = jp.getArgs();
         String detalles = "Método: " + jp.getSignature().getName()
                 + ", args=" + Arrays.toString(args);
 
-
         String entidad = jp.getSignature()
                 .getDeclaringType().getSimpleName();
+
         Long entidadId = null;
         if (result != null) {
             try {
-                Method getId = result.getClass().getMethod("getId");
-                Object id = getId.invoke(result);
-                if (id instanceof Long) {
-                    entidadId = (Long) id;
-                }
-            } catch (Exception ignore) {
-            }
+                Method m = result.getClass().getMethod("getId");
+                Object id = m.invoke(result);
+                if (id instanceof Long) entidadId = (Long) id;
+            } catch (Exception ignored) {}
         }
-
         if (entidadId == null && args != null) {
-            for (Object arg : args) {
-                if (arg instanceof Long) {
-                    entidadId = (Long) arg;
+            for (Object a : args) {
+                if (a instanceof Long) {
+                    entidadId = (Long) a;
                     break;
                 }
                 try {
-                    Method getId = arg.getClass().getMethod("getId");
-                    Object id = getId.invoke(arg);
+                    Method m = a.getClass().getMethod("getId");
+                    Object id = m.invoke(a);
                     if (id instanceof Long) {
                         entidadId = (Long) id;
                         break;
                     }
-                } catch (Exception ignore) {
-                }
+                } catch (Exception ignore) {}
             }
         }
 
+
         String ip = null;
-        RequestAttributes attrs = RequestContextHolder.getRequestAttributes();
+        var attrs = RequestContextHolder.getRequestAttributes();
         if (attrs instanceof ServletRequestAttributes sra) {
             ip = sra.getRequest().getRemoteAddr();
         }
+
 
         bitacoraService.registrar(
                 usuario,
