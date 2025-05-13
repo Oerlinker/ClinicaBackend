c008e8e349c8985168382ce94d9fb7f107328cc1
Oerlinker <oerlinker@gmail.com>
2025-05-12 21:21:31 -0400

    Production 2.7.7 Triaje

--- src/main/java/com/clinicaoftalmologica/clinicaoftalmologicabackend/ClinicOftalmologicaApplication.java
+++ src/main/java/com/clinicaoftalmologica/clinicaoftalmologicabackend/ClinicOftalmologicaApplication.java
@@ -1,9 +1,12 @@
 package com.clinicaoftalmologica.clinicaoftalmologicabackend;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.util.TimeZone;
 
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 @EnableAsync
@@ -11,5 +14,9 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class ClinicOftalmologicaApplication {
     public static void main(String[] args) {
         SpringApplication.run(ClinicOftalmologicaApplication.class, args);
+    }
+    @PostConstruct
+    public void init(){
+        TimeZone.setDefault(TimeZone.getTimeZone("America/La_Paz"));
     }
 }
