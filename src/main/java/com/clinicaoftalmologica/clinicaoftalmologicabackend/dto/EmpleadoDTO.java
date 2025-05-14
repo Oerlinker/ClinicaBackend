45f04c8a7cf71cce33baca0418ccd675046ff162
Oerlinker <oerlinker@gmail.com>
2025-05-13 21:14:15 -0400

    Production 2.8.3 Departamento

--- src/main/java/com/clinicaoftalmologica/clinicaoftalmologicabackend/dto/EmpleadoDTO.java
+++ src/main/java/com/clinicaoftalmologica/clinicaoftalmologicabackend/dto/EmpleadoDTO.java
@@ -1,22 +1,48 @@
 package com.clinicaoftalmologica.clinicaoftalmologicabackend.dto;
 
+import com.clinicaoftalmologica.clinicaoftalmologicabackend.model.Cargo;
+import com.clinicaoftalmologica.clinicaoftalmologicabackend.model.Departamento;
+import com.clinicaoftalmologica.clinicaoftalmologicabackend.model.Especialidad;
+import com.clinicaoftalmologica.clinicaoftalmologicabackend.model.Usuario;
 import com.clinicaoftalmologica.clinicaoftalmologicabackend.model.Empleado;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class EmpleadoDTO {
 
     private Long id;
-    private String nombre;
-    private String apellido;
-
-    public EmpleadoDTO() {}
+    private Usuario usuario;
+    private Cargo cargo;
+    private Especialidad especialidad;
+    private Departamento departamento;
+    private String fechaContratacion;
+    private Double salario;
 
     public EmpleadoDTO(Empleado e) {
         this.id = e.getId();
-        this.nombre = e.getUsuario().getNombre();
-        this.apellido = e.getUsuario().getApellido();
+        this.usuario = e.getUsuario();
+        this.cargo = e.getCargo();
+        this.especialidad = e.getEspecialidad();
+        this.departamento = e.getDepartamento();
+
+
+        LocalDate fecha = e.getFechaContratacion();
+        this.fechaContratacion = (fecha != null)
+                ? fecha.format(DateTimeFormatter.ISO_DATE)
+                : null;
+
+
+        BigDecimal salarioBD = e.getSalario();
+        this.salario = (salarioBD != null) ? salarioBD.doubleValue() : null;
     }
 
     public Long getId() { return id; }
-    public String getNombre() { return nombre; }
-    public String getApellido() { return apellido; }
+    public Usuario getUsuario() { return usuario; }
+    public Cargo getCargo() { return cargo; }
+    public Especialidad getEspecialidad() { return especialidad; }
+    public Departamento getDepartamento() { return departamento; }
+    public String getFechaContratacion() { return fechaContratacion; }
+    public Double getSalario() { return salario; }
 }
