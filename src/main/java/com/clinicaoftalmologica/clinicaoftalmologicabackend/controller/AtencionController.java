62e4e1ec930570776ffd9c9a02b7807c4112e42e
Oerlinker <oerlinker@gmail.com>
2025-06-14 19:44:40 -0400

    Production 3.4.5 Sprint 4

--- src/main/java/com/clinicaoftalmologica/clinicaoftalmologicabackend/controller/AtencionController.java
+++ src/main/java/com/clinicaoftalmologica/clinicaoftalmologicabackend/controller/AtencionController.java
@@ -9,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/atenciones")
 @RequiredArgsConstructor
@@ -23,5 +25,12 @@ public class AtencionController {
         // Convertir la entidad a DTO antes de devolverla
         AtencionResponseDTO responseDTO = new AtencionResponseDTO(atencion);
         return ResponseEntity.ok(responseDTO);
+    }
+
+    @GetMapping("/usuario/{usuarioId}")
+    @PreAuthorize("hasAnyAuthority('DOCTOR','ADMIN','PACIENTE')")
+    public ResponseEntity<List<AtencionResponseDTO>> listarPorUsuario(@PathVariable Long usuarioId) {
+        List<AtencionResponseDTO> list = atencionService.listarPorUsuario(usuarioId);
+        return ResponseEntity.ok(list);
     }
 }
