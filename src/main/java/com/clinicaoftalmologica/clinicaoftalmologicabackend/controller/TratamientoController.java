b4c1e3ed1395956cee07c05b8bfe4af25174060f
oerlinker <oerlinker@gmail.com>
2025-06-18 22:45:17 -0400

    Produccion 3.4.9 Sprint 4

--- src/main/java/com/clinicaoftalmologica/clinicaoftalmologicabackend/controller/TratamientoController.java
+++ src/main/java/com/clinicaoftalmologica/clinicaoftalmologicabackend/controller/TratamientoController.java
@@ -103,7 +103,7 @@ public class TratamientoController {
 
     // Endpoint para eliminar la asociación entre un medicamento y un tratamiento (eliminar una dosis)
     @DeleteMapping("/{id}/medicamentos/{mtId}")
-    @PreAuthorize("hasAuthority('DOCTOR')")
+    @PreAuthorize("hasAnyAuthority('DOCTOR', 'ADMIN')")
     public ResponseEntity<?> eliminarMedicamentoDeTratamiento(
             @PathVariable Long id,
             @PathVariable Long mtId) {
